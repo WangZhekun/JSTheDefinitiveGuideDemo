f126ad2f12e792da7f91df5b12c773158ea2761a
WangZhekun <wzk1992163@163.com>
2016-07-25 15:18:13 +0800

    完成第9章例11，问题：子类无法继承父类构造函数中使用“this.”添加的方法？？

---
+++ chapter9/9.11 inherit.js
@@ -0,0 +1,24 @@
+/**
+ * 定义子类
+ */
+
+/**
+ *
+ * @param superclass 父类的构造函数
+ * @param constructor 新的子类的构造函数
+ * @param methods 实例方法：复制至原型中
+ * @param statics 类属性：复制至构造函数中
+ */
+function defineSubClass(superclass, constructor, methods, statics) {
+    // 建立子类的原型对象
+    constructor.prototype = inherit(superclass.prototype); // 这只能表示子类继承了父类的原型，子类并没有继承父类自有的方法？？？？
+    constructor.prototype.constructor = constructor;
+
+    if(methods) extend(constructor.prototype, methods);
+    if(statics) extend(constructor, statics);
+    return constructor;
+}
+
+Function.prototype.extend = function(constructor, methods, statics) {
+    return defineSubClass(this, constructor, methods, statics);
+};
